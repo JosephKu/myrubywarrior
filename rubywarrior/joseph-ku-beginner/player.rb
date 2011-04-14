f777b8a05499ca6ae148456881ec6a1bf4d5ea18
Joseph Ku <chiehfang.ku@gmail.com>
2011-04-14 17:55:00 +0800

    Finish beginner Level 3.

--- rubywarrior/joseph-ku-beginner/player.rb
+++ rubywarrior/joseph-ku-beginner/player.rb
@@ -1,7 +1,7 @@
 class Player
   def play_turn(warrior)
     if warrior.feel.empty?
-      warrior.walk!
+      warrior.health < 20 ? warrior.rest! : warrior.walk!
     else
       warrior.attack!
     end
