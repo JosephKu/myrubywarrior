74e2a32158ab2e57323ff80fd7a586d27c174f7a
Joseph Ku <chiehfang.ku@gmail.com>
2011-04-14 18:33:02 +0800

    Finish beginner Level 5.

--- rubywarrior/joseph-ku-beginner/player.rb
+++ rubywarrior/joseph-ku-beginner/player.rb
@@ -3,7 +3,7 @@ class Player
     if warrior.feel.empty?
       (warrior.health < 20 && warrior.health >= @health) ? warrior.rest! : warrior.walk!
     else
-      warrior.attack!
+      warrior.feel.captive? ? warrior.rescue! : warrior.attack!
     end
     @health = warrior.health
   end
