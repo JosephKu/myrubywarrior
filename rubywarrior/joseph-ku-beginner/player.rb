6bfb497c2409ec53ee5737dd6ea520c1f613398d
Joseph Ku <chiehfang.ku@gmail.com>
2011-04-15 19:09:54 +0800

    Finish beginner Level 7.

--- rubywarrior/joseph-ku-beginner/player.rb
+++ rubywarrior/joseph-ku-beginner/player.rb
@@ -1,7 +1,13 @@
 class Player
   def play_turn(warrior)
     @health = warrior.health if @health.nil?
-    @captive_rescued.nil? ? @direction = :backward : @direction = :forward
+    @direction = :forward if @direction.nil?  
+
+    if warrior.feel(@direction).wall?
+      warrior.pivot! :backward
+      return
+    end
+
     if warrior.feel(@direction).empty?
       if warrior.health < @health
         warrior.health > 7 ? warrior.walk! : warrior.walk!(:backward) # Magic number 7!
