0c7b232b7b353cbf6da670e5d633c1b3cb925a26
Joseph Ku <chiehfang.ku@gmail.com>
2011-04-17 16:30:20 +0800

    Finish beginner EPIC Mode.

--- rubywarrior/joseph-ku-beginner/player.rb
+++ rubywarrior/joseph-ku-beginner/player.rb
@@ -1,18 +1,33 @@
 class Player
   def play_turn(warrior)
     @health = warrior.health if @health.nil?
-    @direction = :forward if @direction.nil?  
-
+    @direction = :forward if @direction.nil?
+ 
     if warrior.feel(@direction).wall?
       warrior.pivot! :backward
       return
     end
 
+    if captive_behind?(warrior)
+      warrior.pivot! :backward
+      return
+    end
+
+    if archer_behind?(warrior)
+      warrior.walk!(@direction) if warrior.feel(@direction).empty?
+      return
+    end
+
     if warrior.feel(@direction).empty?
+      if no_enemy?(warrior)
+        warrior.walk!
+        return
+      end
+
       if shoot?(warrior)
         warrior.shoot!
       elsif warrior.health < @health
-        warrior.health > 7 ? warrior.walk! : warrior.walk!(:backward) # Magic number 7!
+        warrior.health > 6 ? warrior.walk! : warrior.walk!(:backward)
       elsif warrior.health < 20
         warrior.rest! 
       else
@@ -29,13 +44,36 @@ class Player
   end
 
   def shoot?(warrior)
-    warrior.look(@direction).each do |s|
-      case s.to_s
-      when "Captive"
-        return false
-      when "Wizard"
-        return true
+    warrior.look(@direction).each_with_index do |s, i|
+      if s.enemy?
+        if i == 0
+          return false
+        else
+          return true
+        end
       end
+      return false if s.to_s == "Captive"
+    end
+    return false
+  end
+
+  def captive_behind?(warrior)
+    warrior.look(:backward).each do |s|
+      return true if s.to_s == "Captive"
+    end
+    return false
+  end
+  
+  def no_enemy?(warrior)
+    warrior.look(@direction).each do |s|
+      return false if s.enemy?
+    end
+    return true
+  end
+
+  def archer_behind?(warrior)
+    warrior.look(:backward).each do |s|
+      return true if s.to_s == "Archer"
     end
     return false
   end
